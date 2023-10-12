8b9d25f5f64e30b6202ccf3030676749baf1f058
gurudatt <119776344+gurudattch@users.noreply.github.com>
2023-10-12 10:41:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function dark(){
+    let count = 0
+    document.getElementById('body').style.backgroundColor="black"
+    document.getElementById('btn').style.marginLeft="-30px"
+    document.getElementById('btn').innerHTML = "1";
+    
+}
+function white(){
+    document.getElementById('body').style.backgroundColor="lightslategrey"
+    document.getElementById('btn').style.marginLeft="-15px"
+    document.getElementById('btn').innerHTML = "0";
+}
